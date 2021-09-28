13032b0aeecfe2b6ed5f4dc6fac1cba351611c92
Bo Petersen <bo.soborg.petersen@3shape.com>
2021-09-28 10:15:59 +0200

    Change ingress.class annotations to spec for nginx v.1

--- aks-cli/aks/nginx/upgrade.ps1
+++ aks-cli/aks/nginx/upgrade.ps1
@@ -3,8 +3,16 @@ param($prefix, $configPrefix, $ip, [switch] $oldDnsNamingConvention, [switch] $s
 WriteAndSetUsage ([ordered]@{
     "[prefix]" = "Kubernetes deployment name prefix"
     "[config prefix]" = "AKS-CLI config file name prefix"
+    "[oldDnsNamingConvention]" = "Add '-aks' to dns name"
     "[ip]" = "Azure Public IP to use"
 })
+
+if (!$ip)
+{
+    $svc = "nginx-ingress-ingress-nginx-controller"
+    $svc = ConditionalOperator $prefix "$prefix-$svc" "$svc"
+    $ip = KubectlQuery "get svc $svc" -n ingress -j '{.spec.loadBalancerIP}'
+}
 
 $prefixString = ConditionalOperator $prefix "-prefix '$prefix'"
 $configPrefixString = ConditionalOperator $configPrefix "-configPrefix '$configPrefix'"
